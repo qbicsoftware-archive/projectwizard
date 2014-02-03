648975b82b9c10ef7de2e6367581fbca199d69a7
David Wojnar <david.wojnar@uni-tuebingen.de>
2014-02-03 16:19:04 +0000

    Added use_key_as key to ToppBase. Now other keys can be used as aliases.
    git-svn-id: svn+ssh://sshgw-bs.informatik.uni-tuebingen.de/abi-projects/QBiC/PortalDevelopment/svn@38 a0440ff8-2b04-4d88-a457-8e570ff6c9eb

--- QPort/portalWorkflows/apps/toppTools/ToppBase.py
+++ QPort/portalWorkflows/apps/toppTools/ToppBase.py
@@ -8,25 +8,37 @@ import os
 
 class ToppBase(BasicApp):
 
-    args = [Argument("EXECUTABLE", default='OpenMSInfo'),
+    class keys:
+        EXECUTABLE = "EXECUTABLE"
+        FILE = "FILE"
+        ADDITIONAL_PARAMETERS = "ADDITIONAL_PARAMETERS"
+        OUTPUTPARAMETER = "outputParameter"
+        use_as_key = "use_as_key"
+        INPUT_OUTPUT_RELATIONSHIP = "INPUT_OUTPUT_RELATIONSHIP"
+
+    args = [Argument(keys.EXECUTABLE, default='OpenMSInfo'),
             #Argument("in", help='input file for executable', default=''),
             #Argument("out", help='input file for executable', default=''),
-            Argument('ADDITIONAL_PARAMETERS', help='additional parameter for the executable are forwarded here, e.g. -threads 4', default=''),
-            Argument('INPUT_OUTPUT_RELATIONSHIP', help='inputs and output can have many_to_one(merge) or one_to_one(transform, includes lists) relationships', default='transform')]
+            Argument(keys.ADDITIONAL_PARAMETERS, help='additional parameter for the executable are forwarded here, e.g. -threads 4', default=''),
+            Argument(keys.INPUT_OUTPUT_RELATIONSHIP, help='inputs and output can have many_to_one(merge) or one_to_one(transform, includes lists) relationships', default='transform'),
+            Argument(keys.use_as_key, help="""use_as_key key1 key2. For parameter handling and value of key2
+                                            will be used as it would be the value of key1. Be aware that FILE someKey
+                                            means. That the value of someKey will be used as the value of FILE. Also,
+                                            the output value will be written to FILE.""", default='')]
 
     def _setup_info(self):
         super(ToppBase, self)._setup_info()
-        self.info[Keys.NAME] += '_' + self.info['EXECUTABLE']
+        self.info[Keys.NAME] += '_' + self.info[self.keys.EXECUTABLE]
 
     def prepare_run(self):
         self.log.debug("Preparing")
         parameters = self.parse_parameters()
-        self.command = self.info['EXECUTABLE'] + ' ' + parameters + ' ' + self.info['ADDITIONAL_PARAMETERS']
+        self.command = self.info[self.keys.EXECUTABLE] + ' ' + parameters + ' ' + self.info[self.keys.ADDITIONAL_PARAMETERS]
 
     def validate_run(self):
         super(ToppBase, self).validate_run()
-        self.info['ADDITIONAL_PARAMETERS'] = ''
-        del self.info['outputParameter']
+        self.info[self.keys.ADDITIONAL_PARAMETERS] = ''
+        del self.info[self.keys.OUTPUTPARAMETER]
         #if(self.info.get(Keys.WORKDIR)):
         #    del self.info[Keys.WORKDIR]
         #if self.info['out'] and not os.stat(self.info['out']).st_size > 0:
@@ -35,22 +47,36 @@ class ToppBase(BasicApp):
     def parse_parameters(self):
         """
         Assumes that parameters have the following format: self.info[NAME].someParameter = someValue
-        changes self.info['FILE'] to new result file
+        changes self.info[self.keys.FILE] to new result file or other key if use_as_key parameter in use
         """
         parameters = ''
-        name = self.info['EXECUTABLE']
-        self.info['outputParameter'] = '-out'
+        executable_name = self.info[self.keys.EXECUTABLE]
+        self.info[self.keys.OUTPUTPARAMETER] = '-out'
+        use_as_key_dic = {}
+        info_backup = {}
+        if self.info.get(self.keys.use_as_key):
+            use_as_key = self.info[self.keys.use_as_key]
+            use_as_key_lis = use_as_key.split(' ')
+            nu_use_as_key = len(use_as_key_lis)
+            if not use_as_key or nu_use_as_key % 2 != 0:
+                raise KeyError('number of original and replace keys does not match: ' + str(use_as_key_lis))
+            info_backup = self.info.copy()
+            for i in xrange(0, nu_use_as_key, 2):
+                self.info[use_as_key_lis[i]] = self.info[use_as_key_lis[i+1]]
+                use_as_key_dic[use_as_key_lis[i]] = use_as_key_lis[i+1]
+
         for key in self.info:
             if "delete_this_tag" in key:
                 continue
-            if name in key:
+            if executable_name in key:
                 splitsplit = key.split('.')
                 parameter = ''
-                if len(splitsplit) > 1 and splitsplit[1] == 'outputParameter':
-                    self.info['outputParameter'] = self.info[key].strip()
+                if len(splitsplit) > 1 and splitsplit[1] == self.keys.OUTPUTPARAMETER:
+                    self.info[self.keys.OUTPUTPARAMETER] = self.info[key].strip()
                     continue
                 if len(splitsplit) > 1:
-                    parameter = ''.join((' -', splitsplit[1]))
+                    p = splitsplit[1]
+                    parameter = ''.join((' -', p))
                 option = self.info[key].strip()
                 if (option == 'True'):
                     parameters += parameter
@@ -61,38 +87,54 @@ class ToppBase(BasicApp):
                     else:
                         parameter = ''.join((parameter, ' ', option_env))
                     parameters += parameter
-        if self.info.get('FILE'):
-            parameters = self._parse_input_output(parameters, name)
+        for key in use_as_key_dic:
+            if key in info_backup:
+                self.info[key] = info_backup[key]
+        if self.keys.FILE in use_as_key_dic:
+            self.keys.FILE = use_as_key_dic[self.keys.FILE]
+        if self.info.get(self.keys.FILE):
+            parameters = self._parse_input_output(parameters, executable_name)
         return parameters
 
     def _parse_input_output(self, parameters, name):
-        out = ' ' + self.info['outputParameter'] + ' '
-        if isinstance(self.info['FILE'], list) and self.info['INPUT_OUTPUT_RELATIONSHIP'] == 'transform':
+        out = ' ' + self.info[self.keys.OUTPUTPARAMETER] + ' '
+        outputExtension = ''
+        if name+'.out_delete_this_tag' in self.info:
+            outputExtension = '.' + self.info[name+'.out_delete_this_tag']
+        else:
+            if isinstance(self.info[self.keys.FILE], list):
+                outputExtension = os.path.splitext(os.path.basename(self.info[self.keys.FILE][0]))[1]
+            else:
+                outputExtension = os.path.splitext(os.path.basename(self.info[self.keys.FILE]))[1]
+
+
+        if isinstance(self.info[self.keys.FILE], list) and \
+                      self.info[self.keys.INPUT_OUTPUT_RELATIONSHIP] == 'transform':
             in_files = ''
             out_files_lis = []
             out_files = ''
-            for f in self.info['FILE']:
+            for f in self.info[self.keys.FILE]:
                 in_files += ' ' + f
-                out_file = self.info[Keys.WORKDIR] + os.path.splitext(os.path.basename(f))[0] + '.' + self.info[name+'.out_delete_this_tag']
+                out_file = self.info[Keys.WORKDIR] + os.path.splitext(os.path.basename(f))[0] + outputExtension
                 out_files += ' ' + out_file
                 out_files_lis.append(out_file)
             parameters += ' -in ' + in_files
-            self.info['FILE'] = out_files_lis
+            self.info[self.keys.FILE] = out_files_lis
             parameters += out + out_files
 
-        elif isinstance(self.info['FILE'], list) and self.info['INPUT_OUTPUT_RELATIONSHIP'] == 'merge':
+        elif isinstance(self.info[self.keys.FILE], list) and self.info[self.keys.INPUT_OUTPUT_RELATIONSHIP] == 'merge':
             in_files = ''
-            for f in self.info['FILE']:
+            for f in self.info[self.keys.FILE]:
                 in_files += ' ' + f
-            out_file = self.info[Keys.WORKDIR] + os.path.splitext(os.path.basename(self.info['FILE'][0]))[0] + '.' + self.info[name+'.out_delete_this_tag']
+            out_file = self.info[Keys.WORKDIR] + os.path.splitext(os.path.basename(self.info[self.keys.FILE][0]))[0] + outputExtension
             parameters += ' -in ' + in_files
-            self.info['FILE'] = out_file
+            self.info[self.keys.FILE] = out_file
             parameters += out + out_file
 
-        elif not isinstance(self.info['FILE'], list): # same for transform and merge
-            parameters += ' -in ' + self.info['FILE']
-            self.info['FILE'] = self.info[Keys.WORKDIR] + os.path.splitext(os.path.basename(self.info['FILE']))[0] + '.' + self.info[name+'.out_delete_this_tag']
-            parameters += out + self.info['FILE']
+        elif not isinstance(self.info[self.keys.FILE], list):  # same for transform and merge
+            parameters += ' -in ' + self.info[self.keys.FILE]
+            self.info[self.keys.FILE] = self.info[Keys.WORKDIR] + os.path.splitext(os.path.basename(self.info[self.keys.FILE]))[0] + outputExtension
+            parameters += out + self.info[self.keys.FILE]
 
         return parameters
 
