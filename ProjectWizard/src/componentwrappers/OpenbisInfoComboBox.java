bd8b2c01c5570c7fb80b505d39245184710d147d
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2016-05-13 14:46:17 +0200

    -

--- ProjectWizard/src/componentwrappers/OpenbisInfoComboBox.java
+++ ProjectWizard/src/componentwrappers/OpenbisInfoComboBox.java
@@ -1,19 +1,17 @@
 /*******************************************************************************
- * QBiC Project Wizard enables users to create hierarchical experiments including different study conditions using factorial design.
- * Copyright (C) "2016"  Andreas Friedrich
+ * QBiC Project Wizard enables users to create hierarchical experiments including different study
+ * conditions using factorial design. Copyright (C) "2016" Andreas Friedrich
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 package componentwrappers;
 
@@ -21,6 +19,7 @@ import java.util.List;
 
 import main.ProjectwizardUI;
 
+import com.vaadin.shared.ui.combobox.FilteringMode;
 import com.vaadin.ui.ComboBox;
 
 /**
@@ -39,5 +38,6 @@ public class OpenbisInfoComboBox extends AOpenbisInfoComponent {
   public OpenbisInfoComboBox(String label, String description, List<String> data) {
     super(description, new ComboBox(label, data));
     inner.setStyleName(ProjectwizardUI.boxTheme);
+    ((ComboBox) inner).setFilteringMode(FilteringMode.CONTAINS);
   }
 }
